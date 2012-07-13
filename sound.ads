e1e277e16e38a1a7b994aa3a63d3af246d09c2ac
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2012-07-13 10:33:02 +0200

    48 kHz seems to be the default sample rate with ALSA, so lets use that.

--- sound.ads
+++ sound.ads
@@ -9,5 +9,5 @@
 
 package Sound is
    type Sample_Frequency is range 4_000 .. 196_000;
-   Default_Sample_Frequency : constant Sample_Frequency := 44_100;
+   Default_Sample_Frequency : constant Sample_Frequency := 48_000;
 end Sound;
